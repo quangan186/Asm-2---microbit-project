03d266783d8a20ba0faf3cfd44ab4d10654f3eac
quangan186 <61352012+quangan186@users.noreply.github.com>
2021-11-17 22:29:58 +0700

    Introduction

--- main.ts
+++ main.ts
@@ -1,4 +1,27 @@
-basic.showString("Welcome to RPS game!")
-basic.forever(function () {
-	
-})
+music.startMelody(music.builtInMelody(Melodies.Prelude), MelodyOptions.OnceInBackground)
+music.setVolume(127)
+basic.showString("R")
+basic.pause(700)
+basic.showLeds(`
+    . . . . .
+    . . . # #
+    # # # # #
+    . . . # #
+    . . . . .
+    `)
+basic.pause(1000)
+basic.showString("P")
+basic.pause(700)
+basic.showIcon(IconNames.Square)
+basic.pause(1000)
+basic.showString("S")
+basic.pause(700)
+basic.showIcon(IconNames.Scissors)
+basic.pause(1000)
+basic.showLeds(`
+    . . . . .
+    . . . . .
+    . . . . .
+    . . . . .
+    . . . . .
+    `)
