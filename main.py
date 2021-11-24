858bea159a653433f26832a5f27a4919040a901b
quangan186 <61352012+quangan186@users.noreply.github.com>
2021-11-24 13:21:39 +0700

    Update pxt.json, main.blocks, main.ts, main.py

---
+++ main.py
@@ -0,0 +1,191 @@
+def on_pin_pressed_p0():
+    global pause2
+    pause2 = 0
+input.on_pin_pressed(TouchPin.P0, on_pin_pressed_p0)
+
+def compare():
+    global scoreP, scoreC
+    if playerhand < hand:
+        if playerhand == 1 and hand == 3:
+            music.play_melody("C D E C G - - - ", 120)
+            basic.show_string("WIN")
+            scoreP += 1
+        else:
+            music.play_melody("G E E D C - - - ", 120)
+            basic.show_string("LOSE")
+            scoreC += 1
+    elif playerhand == hand:
+        music.play_melody("E E - - - - - - ", 120)
+        basic.show_string("DRAW")
+    elif playerhand == 3 and hand == 1:
+        music.play_melody("G E E D C - - - ", 120)
+        scoreC += 1
+        basic.show_string("LOSE")
+    else:
+        music.play_melody("C D E C G - - - ", 120)
+        scoreP += 1
+        basic.show_string("WIN")
+    basic.show_string("C")
+    basic.show_string(":")
+    basic.show_number(scoreC)
+    basic.show_string("P")
+    basic.show_string(":")
+    basic.show_number(scoreP)
+def countdown():
+    global index2, current_index, pause2
+    index2 = 6
+    current_index = 0
+    pause2 = 0
+    while index2 > 0:
+        if pause2 == 0:
+            index2 = index2 - 1
+            if index2 == 0:
+                music.play_tone(392, music.beat(BeatFraction.DOUBLE))
+            else:
+                music.play_tone(392, music.beat(BeatFraction.HALF))
+            basic.show_number(index2)
+        else:
+            while index2 > 0:
+                current_index = index2 + 1
+                basic.show_string("PAUSE")
+                if pause2 == 0:
+                    current_index = current_index - 1
+                    if current_index == 0:
+                        music.play_tone(392, music.beat(BeatFraction.DOUBLE))
+                    else:
+                        music.play_tone(392, music.beat(BeatFraction.HALF))
+                    basic.show_number(current_index)
+                    break
+                break
+
+def on_button_pressed_a():
+    global playerhand
+    playerhand = 1
+    basic.show_icon(IconNames.SCISSORS)
+input.on_button_pressed(Button.A, on_button_pressed_a)
+
+def playerBot():
+    global hand
+    basic.pause(500)
+    hand = randint(1, 3)
+    if hand == 1:
+        basic.show_icon(IconNames.SCISSORS)
+    elif hand == 2:
+        basic.show_leds("""
+            . . . . .
+                        . . . # #
+                        # # # # #
+                        . . . # #
+                        . . . . .
+        """)
+    else:
+        basic.show_icon(IconNames.SQUARE)
+
+def on_pin_pressed_p2():
+    history()
+input.on_pin_pressed(TouchPin.P2, on_pin_pressed_p2)
+
+def on_button_pressed_ab():
+    global playerhand
+    playerhand = 3
+    basic.show_icon(IconNames.SQUARE)
+input.on_button_pressed(Button.AB, on_button_pressed_ab)
+
+def history():
+    global win, lose
+    win = 0
+    lose = 0
+    for res in history_list:
+        if res == 0:
+            lose += history_list.count(res)
+        else:
+            win += history_list.count(res)
+    basic.show_string("HISTORY")
+    basic.show_string("W")
+    basic.show_string(":")
+    basic.pause(500)
+    basic.show_string("L")
+    basic.show_string(":")
+    basic.pause(500)
+    basic.clear_screen()
+
+def on_button_pressed_b():
+    global playerhand
+    playerhand = 2
+    basic.show_leds("""
+        . . . . .
+                . . . # #
+                # # # # #
+                . . . # #
+                . . . . .
+    """)
+input.on_button_pressed(Button.B, on_button_pressed_b)
+
+def on_pin_pressed_p1():
+    global pause2
+    pause2 = 1
+input.on_pin_pressed(TouchPin.P1, on_pin_pressed_p1)
+
+def on_gesture_shake():
+    gameplay()
+input.on_gesture(Gesture.SHAKE, on_gesture_shake)
+
+def gameplay():
+    global playerhand, result
+    list2: List[number] = []
+    while scoreC <= 2 and scoreP <= 2:
+        playerhand = 0
+        countdown()
+        playerBot()
+        compare()
+        if scoreC == 2:
+            result = 0
+            basic.show_string("YOU LOSE")
+            basic.clear_screen()
+            break
+        if scoreP == 2:
+            result = 1
+            basic.show_string("YOU WIN")
+            basic.clear_screen()
+            break
+    list2.append(result)
+result = 0
+lose = 0
+win = 0
+current_index = 0
+index2 = 0
+scoreC = 0
+scoreP = 0
+hand = 0
+playerhand = 0
+pause2 = 0
+history_list: List[number] = []
+history_list = []
+music.start_melody(music.built_in_melody(Melodies.PRELUDE),
+    MelodyOptions.ONCE_IN_BACKGROUND)
+music.set_volume(127)
+basic.show_string("R")
+basic.pause(700)
+basic.show_leds("""
+    . . . . .
+        . . . # #
+        # # # # #
+        . . . # #
+        . . . . .
+""")
+basic.pause(1000)
+basic.show_string("P")
+basic.pause(700)
+basic.show_icon(IconNames.SQUARE)
+basic.pause(1000)
+basic.show_string("S")
+basic.pause(700)
+basic.show_icon(IconNames.SCISSORS)
+basic.pause(1000)
+basic.show_leds("""
+    . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+""")
--- main.ts
+++ main.ts
@@ -1,3 +1,6 @@
+input.onPinPressed(TouchPin.P0, function () {
+    pause2 = 0
+})
 function compare () {
     if (playerhand < hand) {
         if (playerhand == 1 && hand == 3) {
@@ -12,16 +15,14 @@ function compare () {
     } else if (playerhand == hand) {
         music.playMelody("E E - - - - - - ", 120)
         basic.showString("DRAW")
+    } else if (playerhand == 3 && hand == 1) {
+        music.playMelody("G E E D C - - - ", 120)
+        scoreC += 1
+        basic.showString("LOSE")
     } else {
-        if (playerhand == 3 && hand == 1) {
-            music.playMelody("G E E D C - - - ", 120)
-            basic.showString("LOSE")
-            scoreC += 1
-        } else {
-            music.playMelody("C D E C G - - - ", 120)
-            basic.showString("WIN")
-            scoreP += 1
-        }
+        music.playMelody("C D E C G - - - ", 120)
+        scoreP += 1
+        basic.showString("WIN")
     }
     basic.showString("C")
     basic.showString(":")
@@ -29,6 +30,38 @@ function compare () {
     basic.showString("P")
     basic.showString(":")
     basic.showNumber(scoreP)
+}
+function countdown () {
+    index2 = 6
+    current_index = 0
+    pause2 = 0
+    while (index2 > 0) {
+        if (pause2 == 0) {
+            index2 = index2 - 1
+            if (index2 == 0) {
+                music.playTone(392, music.beat(BeatFraction.Whole))
+            } else {
+                music.playTone(392, music.beat(BeatFraction.Half))
+            }
+            basic.showNumber(index2)
+        } else {
+            while (index2 > 0) {
+                current_index = index2 + 1
+                basic.showString("PAUSE")
+                if (pause2 == 0) {
+                    current_index = current_index - 1
+                    if (current_index == 0) {
+                        music.playTone(392, music.beat(BeatFraction.Whole))
+                    } else {
+                        music.playTone(392, music.beat(BeatFraction.Half))
+                    }
+                    basic.showNumber(current_index)
+                    break;
+                }
+                break;
+            }
+        }
+    }
 }
 input.onButtonPressed(Button.A, function () {
     playerhand = 1
@@ -51,10 +84,35 @@ function playerBot () {
         basic.showIcon(IconNames.Square)
     }
 }
+input.onPinPressed(TouchPin.P2, function () {
+    history()
+})
 input.onButtonPressed(Button.AB, function () {
     playerhand = 3
     basic.showIcon(IconNames.Square)
 })
+function history () {
+    basic.showString("HISTORY")
+    win = 0
+    lose = 0
+    for (let res of history_list) {
+        if (res == 0) {
+            lose += _py.py_array_count(history_list, res)
+        } else {
+            win += _py.py_array_count(history_list, res)
+        }
+    }
+    basic.showString("HISTORY")
+    basic.showString("W")
+    basic.showString(":")
+    basic.showNumber(win)
+    basic.pause(500)
+    basic.showString("L")
+    basic.showString(":")
+    basic.showNumber(lose)
+    basic.pause(500)
+    basic.clearScreen()
+}
 input.onButtonPressed(Button.B, function () {
     playerhand = 2
     basic.showLeds(`
@@ -65,46 +123,48 @@ input.onButtonPressed(Button.B, function () {
         . . . . .
         `)
 })
+input.onPinPressed(TouchPin.P1, function () {
+    pause2 = 1
+})
 input.onGesture(Gesture.Shake, function () {
     gameplay()
 })
-function count_Down () {
-    timer = 6
-    for (let index = 0; index < 6; index++) {
-        music.playTone(392, music.beat(BeatFraction.Eighth))
-        timer += -1
-        basic.showNumber(timer)
-        basic.pause(1000)
-        if (timer == 1) {
-            music.playTone(392, music.beat(BeatFraction.Breve))
-        }
-        if (timer == 0) {
-            music.stopAllSounds()
-        }
-    }
-}
 function gameplay () {
-    while (scoreC <= 2 && scoreP <= 2) {
-        count_Down()
+    basic.showString("BEST OF 3")
+    scoreC = 0
+    scoreP = 0
+    while (scoreC <= 2 || scoreP <= 2) {
+        playerhand = 0
+        countdown()
         playerBot()
         compare()
         if (scoreC == 2) {
+            result = 0
             basic.showString("YOU LOSE")
             basic.clearScreen()
             break;
         }
         if (scoreP == 2) {
+            result = 1
             basic.showString("YOU WIN")
             basic.clearScreen()
             break;
         }
     }
+    history_list.push(result)
 }
-let timer = 0
+let result = 0
+let lose = 0
+let win = 0
+let current_index = 0
+let index2 = 0
 let scoreC = 0
 let scoreP = 0
 let hand = 0
 let playerhand = 0
+let pause2 = 0
+let history_list : number[] = []
+history_list = []
 music.startMelody(music.builtInMelody(Melodies.Prelude), MelodyOptions.OnceInBackground)
 music.setVolume(127)
 basic.showString("R")
